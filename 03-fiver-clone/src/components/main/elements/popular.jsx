da9dd1c08199626af9f1471f03b8f57fce38f0f7
Jit Mukherjee <133637574+GTAJIT@users.noreply.github.com>
2025-02-05 16:44:14 +0530

    popular section started

--- 03-fiver-clone/src/components/main/elements/popular.jsx
+++ 03-fiver-clone/src/components/main/elements/popular.jsx
@@ -1,7 +1,57 @@
 function Popular() {
     return (
         <div className="pop">
-            
+            <div className="pop-text">Popular services</div>
+            <div className="pop-section">
+                <div className="pop-section-data">
+                    <p className="pop-text capitalize">website development</p>
+                    <img className="pop-img" src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_188,dpr_1.0/v1/attachments/generic_asset/asset/798403f5b92b1b5af997acc704a3d21c-1702465156477/website-development.png" alt="pop-404" />
+                </div>
+                <div className="pop-section-data">
+                    <p className="pop-text capitalize">logo design</p>
+                    <img className="pop-img" src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_188,dpr_1.0/v1/attachments/generic_asset/asset/798403f5b92b1b5af997acc704a3d21c-1702465156494/logo-design.png" alt="pop-404" />
+                </div>
+                <div className="pop-section-data">
+                    <p className="pop-text capitalize">SEO</p>
+                    <img className="pop-img" src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_188,dpr_1.0/v1/attachments/generic_asset/asset/798403f5b92b1b5af997acc704a3d21c-1702465156488/seo.png" alt="pop-404" />
+                </div>
+                <div className="pop-section-data">
+                    <p className="pop-text capitalize">architecture & interior design</p>
+                    <img className="pop-img" src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_188,dpr_1.0/v1/attachments/generic_asset/asset/798403f5b92b1b5af997acc704a3d21c-1702465156473/architecture-design.png" alt="pop-404" />
+                </div>
+                <div className="pop-section-data">
+                    <p className="pop-text capitalize">social media marketing</p>
+                    <img className="pop-img" src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_188,dpr_1.0/v1/attachments/generic_asset/asset/798403f5b92b1b5af997acc704a3d21c-1702465156476/social-media-marketing.png" alt="pop-404" />
+                </div>
+                <div className="pop-section-data">
+                    <p className="pop-text capitalize">voice over</p>
+                    <img className="pop-img" src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_188,dpr_1.0/v1/attachments/generic_asset/asset/798403f5b92b1b5af997acc704a3d21c-1702465156479/voice-over.png" alt="pop-404" />
+                </div>
+                <div className="pop-section-data">
+                    <p className="pop-text capitalize">UGC videos</p>
+                    <img className="pop-img" src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_188,dpr_1.0/v1/attachments/generic_asset/asset/ece24f7f595e2dd44b26567705d1c600-1728279781879/UGC%20Video%20img.png" alt="pop-404" />
+                </div>
+                <div className="pop-section-data">
+                    <p className="pop-text capitalize">software development</p>
+                    <img className="pop-img" src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_188,dpr_1.0/v1/attachments/generic_asset/asset/798403f5b92b1b5af997acc704a3d21c-1702465156476/software-development.png" alt="pop-404" />
+                </div>
+                <div className="pop-section-data">
+                    <p className="pop-text capitalize">data science & ML</p>
+                    <img className="pop-img" src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_188,dpr_1.0/v1/attachments/generic_asset/asset/798403f5b92b1b5af997acc704a3d21c-1702465156495/data-science.png" alt="pop-404" />
+                </div>
+                <div className="pop-section-data">
+                    <p className="pop-text capitalize">product photography</p>
+                    <img className="pop-img" src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_188,dpr_1.0/v1/attachments/generic_asset/asset/798403f5b92b1b5af997acc704a3d21c-1702465156481/product-photography.png" alt="pop-404" />
+                </div>
+                <div className="pop-section-data">
+                    <p className="pop-text capitalize">E-Commerce marketing</p>
+                    <img className="pop-img" src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_188,dpr_1.0/v1/attachments/generic_asset/asset/798403f5b92b1b5af997acc704a3d21c-1702465156474/e-commerce.png" alt="pop-404" />
+                </div>
+                <div className="pop-section-data">
+                    <p className="pop-text capitalize">video editing</p>
+                    <img className="pop-img" src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_188,dpr_1.0/v1/attachments/generic_asset/asset/798403f5b92b1b5af997acc704a3d21c-1702465156494/video-editing.png" alt="pop-404" />
+                </div>
+            </div>
         </div>
     )
 }
